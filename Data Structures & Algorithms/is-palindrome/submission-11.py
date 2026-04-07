f0345b32f20ec4260ce7dc901ec4cdba27105100
KikiaDia <104317306+KikiaDia@users.noreply.github.com>
2026-04-07 22:16:15 +0200

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = [caracter.lower() for caracter in s if caracter.isalnum()]
+        j = -1
+        for i in range(len(s)) :
+            if s[i] != s[j]:
+                return False
+            j -= 1
+        return True
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = [caracter.lower() for caracter in s if caracter.isalnum()]
+#         if s == s[::-1] :
+#             return True
+#         return False
+
+
+                
