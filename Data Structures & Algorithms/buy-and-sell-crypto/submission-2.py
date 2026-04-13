3871f2c3e143f222bbe22509cc35b8328e323d8f
KikiaDia <104317306+KikiaDia@users.noreply.github.com>
2026-04-13 20:20:01 +0200

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,21 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         profits = []
+#         for i in range(len(prices)) :
+#             for j in range(i+1, len(prices)) :
+#                 profits.append(prices[j]- prices[i]) 
+#         if len(prices) > 1 and max(profits) > 0 :
+#             return max(profits)
+#         return 0
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profits = []
+        if len(prices) > 1 :
+            for i in range(len(prices)-1) :
+                profits.append(max(prices[i+1:]) - prices[i])
+            if max(profits) > 0 :
+                return max(profits)
+        return 0
+
+
